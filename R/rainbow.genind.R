08a6810e76eb9a352d3a46803b6fabb4b1b26456
Chris Brauer <chrisbrauer@cbimac.local>
2018-02-22 08:43:44 +1030

    Initial commit.

---
+++ R/rainbow.genind.R
@@ -0,0 +1,13 @@
+#' ddRAD data from Melanotaenia fluviatilis in the Murray-Darling Basin
+#'
+#' This is a subset of 500 SNPs from the M. fluviatilis riverscape genomics study
+#'
+#'
+#'
+#' @examples
+#' data(rainbow.genind)
+#' dapc <- dapc(rainbow.genind, pop=rainbow.genind$pop)
+#' scatter(dapc)
+
+#' @format A genind object with 500 loci, 249 individuals from 14 populations
+"rainbow.genind"
---
+++ R/test_dapc.R
@@ -0,0 +1,15 @@
+#' Run test DAPC on rainbow.genind
+#'
+#' This function tests the package by running a DAPC on the example data
+#' @param x A genind object
+#' @keywords dapc
+#' @export
+#' @examples
+#' data(rainbow.genind)
+#' test_dapc(rainbow.genind)
+
+
+test_dapc <- function(x){
+  a <- dapc(x, pop=x$pop, n.pca = 30, n.da = 2)
+  scatter(a)
+}
