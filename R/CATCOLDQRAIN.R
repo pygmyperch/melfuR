99efc27b73099c12e60226431b799445367a7683
Chris Brauer <chrisbrauer@cbimac.local>
2018-02-23 16:24:23 +1030

    update

---
+++ R/CATCOLDQRAIN.R
@@ -0,0 +1,9 @@
+#' environmental data describing rainfall during the coldest quarter for Melanotaenia fluviatilis sampling sites in the Murray-Darling Basin
+#'
+#'
+#'
+#' @examples
+#' data(CATCOLDQRAIN)
+#'
+#' @format A text file with one value per population
+"CATCOLDQRAIN"
