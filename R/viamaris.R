d32d1a8bf9d65fe68297dcaa45f35b0d2fc67bc6
cb <pygmyperch@gmail.com>
2019-04-11 16:15:49 +0930

    Update viamaris.R

--- R/viamaris.R
+++ R/viamaris.R
@@ -31,19 +31,6 @@
 
 viamaris <- function (sampleXY, extent.buffer = NULL, resolution = NULL, EPSG = NULL) {
 
-  # check for required packages and install if nec.
-  depnds <- function(x){
-    for( i in x ){
-      #  require returns TRUE invisibly if it was able to load package
-      if( ! require( i , character.only = TRUE ) ){
-        #  If package was not able to be loaded then re-install
-        install.packages( i , dependencies = TRUE )
-        #  Load package after installing
-        require( i , character.only = TRUE )
-      }
-    }
-  }
-  depnds(c("sp", "rgdal", "rgeos", "raster", "gdistance", "maptools"))
 
   # define some variables
   inds <- as.data.frame(sampleXY$ID)
