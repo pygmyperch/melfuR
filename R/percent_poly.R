15298e1793cd222542fbea94d02a0a52afd1fba7
cb <pygmyperch@gmail.com>
2020-10-06 18:37:58 +1030

    Update percent_poly.R

--- R/percent_poly.R
+++ R/percent_poly.R
@@ -21,6 +21,7 @@ percent_poly <- function(genind) {
   poly.lst <- seppop(genind)
   percent.poly <- lapply(poly.lst, function(x) {length(which(nAll(x)==2))/(length(which(nAll(x)==1)) + length(which(nAll(x)==2)))*100})
   percent.poly <- data.frame(round(sapply(percent.poly,c), digits = 2))
+  colnames(percent.poly) <- "%poly"
   return(percent.poly)
   
 }
