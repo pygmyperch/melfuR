e1f2b2f8ad0adbd3fb9789df6a9f90b2f3165c60
cb <pygmyperch@gmail.com>
2020-10-06 18:16:38 +1030

    add function

---
+++ R/percent_poly.R
@@ -0,0 +1,29 @@
+#' This function calculates the % polymorphic loci per population from a genind object
+#'
+#' @param input.file A genind object
+#' @keywords genetic diversity polymorphic loci
+#' @export
+#' @examples
+#'  ## set directory for results to be written
+#'  setwd("path/to/working/directory")
+#'
+#'  ## load the example data
+#'  data(rainbow.genind)
+#'
+#'  # calculate the % polymorphic loci per population
+#'  res <- percent_poly(rainbow.genind)
+#'
+
+
+
+percent_poly <- function(genind) {
+  
+  poly.lst <- seppop(genind)
+  percent.poly <- lapply(poly.lst, function(x) {length(which(nAll(x)==2))/(length(which(nAll(x)==1)) + length(which(nAll(x)==2)))*100})
+  percent.poly <- data.frame(round(sapply(percent.poly,c), digits = 2))
+  return(percent.poly)
+  
+}
+
+
+
