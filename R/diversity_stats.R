4e11670d2895762182c1e116b741eb8045f35835
cb <pygmyperch@gmail.com>
2023-03-29 15:12:02 +1030

    fixed bug in diversity_stats Fis calc

--- R/diversity_stats.R
+++ R/diversity_stats.R
@@ -17,43 +17,43 @@
 
 
 diversity_stats <- function(genind) {
-  
+
   f.stat <- genind2hierfstat(genind)
   # get number of inds per pop
   pop.numbers <- as.data.frame(table(f.stat$pop))
   pop.numbers <- as.data.frame(pop.numbers$Freq)
-  
+
   # estimate allelic richness
   Na <- allelic.richness(f.stat)
   popNa <- as.data.frame(colMeans(Na$Ar, na.rm = TRUE))
-  
-  
+
+
   # estimeate Ho, He, Fis
   stats <- basic.stats(f.stat, digits = 4)
   Ho <- as.data.frame(stats$Ho)
   popHo <- as.data.frame(colMeans(Ho, na.rm = TRUE))
   He <- as.data.frame(stats$Hs)
   popHe <- as.data.frame(colMeans(He, na.rm = TRUE))
-  Fis <- as.data.frame(stats$Fis)
-  popFis <- as.data.frame(colMeans(Fis, na.rm = TRUE))
-  
+  #Fis <- as.data.frame(stats$Fis)
+  popFis <- as.data.frame(1-colSums(stats$Ho,na.rm=TRUE)/colSums(stats$Hs,na.rm=TRUE))
+
   # estimate 95%CIs around the Fis values
   geno.fis <- boot.ppfis(f.stat)
   FisCI <- as.data.frame(geno.fis$fis.ci)
-  
-  
+
+
   # split input into list of data.frames by pop factor and calc %poly loci
   poly.lst <- seppop(genind)
   percent.poly <- lapply(poly.lst, function(x) {(sum(isPoly(x)))/(dim(x@tab)[2]/2-(length(which(nAll(x)==0))))*100})
   percent.poly <- data.frame(sapply(percent.poly,c))
-  
-  
+
+
   geno.stats <- cbind(pop.numbers, popNa, percent.poly, popHo, popHe[,1], popFis[,1], FisCI)
   colnames(geno.stats) <- c("N", "Na", "%poly", "Ho", "He", "Fis", "LCI", "UCI")
   write.csv(geno.stats, "stats.csv")
-  
-  
-  
+
+
+
 }
 
 
---
+++ R/impute.genotypes.R
@@ -0,0 +1,92 @@
+#' impute missing genotypes based on pop structure characterised using snmf
+#'
+#'
+#' @param input.file A genind object
+#' @param K the number of predetermined clusters in your data (suggest using smnf, ADMIXTURE or similar to explore first)
+#' @param no_cores the number of cores to use for the analysis
+#'
+#' @return imputed.genind               a genind object with missing data imputed
+#'
+#'
+#' @author Chris Brauer
+#' @keywords genotype imputation
+#' @export
+#' @examples
+#'  ## set directory for results to be written
+#'  setwd("path/to/working/directory")
+#'
+#'  # load example coordinate file
+#'  data(rainbow.genind)
+#'
+#'  # run analysis
+#'  imputed.gi <- impute.data(rainbow.genind)
+
+
+
+impute.data <- function(input.file, K, no_cores) {
+
+  #check required packages are installed and loaded
+  packages <- c("LEA", "pegas", "plyr", "foreach", "doParallel")
+
+  for(package in packages){
+
+    # if package is installed locally, load
+    if(package %in% rownames(installed.packages()))
+      do.call('require', list(package))
+
+    else {
+      stop("some required packages not loaded")
+    }
+  }
+
+
+
+  alleles <- MC_dart.gi@tab
+  snps <- alleles[,seq(1,ncol(alleles),2)]
+  colnames(snps) <- locNames(MC_dart.gi)
+
+
+  md <- round((sum(is.na(snps)))/(dim(snps)[1]*dim(snps)[2])*100, digits = 2)
+  cat(paste0("Total missing data = "),md,"%\n")
+
+  write.lfmm(snps, "NER.lfmm")
+  lfmm.obj = read.lfmm("NER.lfmm")
+
+  project.snmf = snmf("NER.lfmm", K = K,
+                      entropy = TRUE, repetitions = 10, seed = 42, CPU = no_cores,
+                      project = "new")
+
+  # select the run with the lowest cross-entropy value
+  best = which.min(cross.entropy(project.snmf, K = K))
+
+  # Impute the missing genotypes
+  impute(project.snmf, "NER.lfmm", method = 'mode', K = K, run = best)
+
+  imputed.snps <- read.lfmm("NER.lfmm_imputed.lfmm")
+
+
+  dim(imputed.snps)
+  #imputed.snps[1:10,1:10]
+  colnames(imputed.snps) <- locNames(MC_dart.gi)
+  rownames(imputed.snps) <- indNames(MC_dart.gi)
+  # check total % missing data
+  md2 <- (sum(is.na(imputed.snps)))/(dim(imputed.snps)[1]*dim(imputed.snps)[2])*100
+  cat(paste0("Total missing data now = "),md2,"%\n")
+
+
+  imputed.snps2 <- 2-imputed.snps
+
+  #create indexes for the desired order
+  x <- order(c(1:ncol(imputed.snps), 1:ncol(imputed.snps2)))
+
+  #cbind d1 and d2, interleaving columns with x
+  dat <- cbind(imputed.snps, imputed.snps2)[,x]
+  colnames(dat) <- colnames(MC_dart.gi@tab)
+
+
+  imputed.genind <- genind
+  imputed.genind@tab <- dat
+  mode(imputed.genind@tab) <- "integer"
+
+  return(imputed.genind)
+}
