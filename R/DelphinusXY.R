ad78441b592ecbf1a7309f16671724b0c6d32163
Chris Brauer <pygmyperch@users.noreply.github.com>
2024-03-12 10:50:04 +1100

    Fix viamaris example data

---
+++ R/DelphinusXY.R
@@ -0,0 +1,15 @@
+#' Sample locations of common dolphins in Australia and New Zealand
+#'
+#'
+#'@format A data frame with 21 rows and 3 columns:
+#' \describe{
+#'   \item{ID}{Site name}
+#'   \item{X}{Longitude}
+#'   \item{Y}{Latitude}
+#' }
+#' 
+#' @examples
+#' data(DelphinusXY)
+#' head(yourData)
+#'
+NULL
