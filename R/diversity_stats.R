0fe43adaafff920c1d56d74a7cb74f9892dbe74a
Chris Brauer <pygmyperch@gmail.com>
2024-06-28 09:06:52 +0930

    add write_list_to_excel.R

--- R/diversity_stats.R
+++ R/diversity_stats.R
@@ -1,6 +1,6 @@
-#' This function calculates basic genetic diversity stats from a genind object
+#' This function calculates basic genetic diversity stats from a genind or genlight object
 #'
-#' @param input.file A genind object
+#' @param input.file A genind or genlight object
 #' @examples
 #'  ## set directory for results to be written
 #'  setwd("path/to/working/directory")
@@ -14,47 +14,49 @@
 #' @export
 #' @importFrom hierfstat genind2hierfstat allelic.richness basic.stats boot.ppfis
 #' @importFrom adegenet seppop isPoly nAll
+#' @importFrom dartR.base gl2gi
 
-
-diversity_stats <- function(genind) {
-
+diversity_stats <- function(input.file) {
+  # Check if input is a genlight object and convert to genind if necessary
+  if (inherits(input.file, "genlight")) {
+    genind <- dartR.base::gl2gi(input.file)
+  } else if (inherits(input.file, "genind")) {
+    genind <- input.file
+  } else {
+    stop("Input file must be a genind or genlight object.")
+  }
+  
   f.stat <- genind2hierfstat(genind)
   # get number of inds per pop
   pop.numbers <- as.data.frame(table(f.stat$pop))
   pop.numbers <- as.data.frame(pop.numbers$Freq)
-
+  
   # estimate allelic richness
   Na <- allelic.richness(f.stat)
   popNa <- as.data.frame(colMeans(Na$Ar, na.rm = TRUE))
-
-
-  # estimeate Ho, He, Fis
+  
+  # estimate Ho, He, Fis
   stats <- basic.stats(f.stat, digits = 4)
   Ho <- as.data.frame(stats$Ho)
   popHo <- as.data.frame(colMeans(Ho, na.rm = TRUE))
   He <- as.data.frame(stats$Hs)
   popHe <- as.data.frame(colMeans(He, na.rm = TRUE))
-  #Fis <- as.data.frame(stats$Fis)
-  popFis <- as.data.frame(1-colSums(stats$Ho,na.rm=TRUE)/colSums(stats$Hs,na.rm=TRUE))
-
+  popFis <- as.data.frame(1 - colSums(stats$Ho, na.rm = TRUE) / colSums(stats$Hs, na.rm = TRUE))
+  
   # estimate 95%CIs around the Fis values
   geno.fis <- boot.ppfis(f.stat)
   FisCI <- as.data.frame(geno.fis$fis.ci)
-
-
+  
   # split input into list of data.frames by pop factor and calc %poly loci
   poly.lst <- seppop(genind)
-  percent.poly <- lapply(poly.lst, function(x) {(sum(isPoly(x)))/(dim(x@tab)[2]/2-(length(which(nAll(x)==0))))*100})
-  percent.poly <- data.frame(sapply(percent.poly,c))
-
-
-  geno.stats <- cbind(pop.numbers, popNa, percent.poly, popHo, popHe[,1], popFis[,1], FisCI)
+  percent.poly <- lapply(poly.lst, function(x) {
+    (sum(isPoly(x))) / (dim(x@tab)[2] / 2 - (length(which(nAll(x) == 0)))) * 100
+  })
+  percent.poly <- data.frame(sapply(percent.poly, c))
+  
+  geno.stats <- cbind(pop.numbers, popNa, percent.poly, popHo, popHe[, 1], popFis[, 1], FisCI)
   colnames(geno.stats) <- c("N", "Na", "%poly", "Ho", "He", "Fis", "LCI", "UCI")
   write.csv(geno.stats, "stats.csv")
-
-
-
+  
+  return(geno.stats)
 }
-
-
-
---
+++ R/write_list_to_excel.R
@@ -0,0 +1,48 @@
+#' Write a List of Data Frames to an Excel File
+#'
+#' This function writes a list of data frames to an Excel file, with each element of the list being written to a separate sheet.
+#'
+#' @param data_list A named list of data frames to be written to the Excel file. Each list element will be written to a separate sheet.
+#' @param file_name A character string specifying the name of the output Excel file.
+#'
+#' @return This function does not return a value. It writes the list of data frames to an Excel file.
+#'
+#' @importFrom openxlsx createWorkbook addWorksheet writeData saveWorkbook
+#'
+#' @examples
+#' \dontrun{
+#' library(openxlsx)
+#'
+#' # Create example data
+#' data1 <- data.frame(A = 1:5, B = letters[1:5])
+#' data2 <- data.frame(X = rnorm(5), Y = runif(5))
+#'
+#' # Create a list of data frames
+#' data_list <- list(Sheet1 = data1, Sheet2 = data2)
+#'
+#' # Write the list to an Excel file
+#' write_list_to_excel(data_list, "example.xlsx")
+#' }
+#' @export
+write_list_to_excel <- function(data_list, file_name) {
+  # Create a new workbook
+  wb <- createWorkbook()
+  
+  # Iterate over each element in the list
+  for (sheet_name in names(data_list)) {
+    # Add a new sheet to the workbook
+    addWorksheet(wb, sheet_name)
+    
+    # Ensure the data is a data frame before writing to Excel
+    data_to_write <- data_list[[sheet_name]]
+    if (!is.data.frame(data_to_write)) {
+      data_to_write <- as.data.frame(data_to_write)
+    }
+    
+    # Write the data to the sheet
+    writeData(wb, sheet_name, data_to_write, rowNames = TRUE)
+  }
+  
+  # Save the workbook to file
+  saveWorkbook(wb, file_name, overwrite = TRUE)
+}
